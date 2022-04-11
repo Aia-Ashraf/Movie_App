d12646aaa0939078fb275c1489a5e58f9c5247f6
Aia Ashraf <aia.ashraf@areebgroup.com>
2022-04-11 22:02:06 +0200

    update gradle.properties file

--- app/src/main/java/com/aashraf/movieapp/data/remote/MoviesAPI.kt
+++ app/src/main/java/com/aashraf/movieapp/data/remote/MoviesAPI.kt
@@ -1,10 +1,11 @@
 package com.aashraf.movieapp.data.remote
 
+import com.aashraf.movieapp.BuildConfig
 import com.aashraf.movieapp.data.entities.MovieListResult
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MoviesAPI {
     @GET("movie/popular")
-    suspend fun getPopularMovies( @Query("api_key") accessKey: String = "c50f5aa4e7c95a2a553d29b81aad6dd0"): MovieListResult
+    suspend fun getPopularMovies( @Query("api_key") accessKey: String = BuildConfig.API_AUTH_KEY): MovieListResult
 }
